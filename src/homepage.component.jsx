5d12b6d14de2a1f342b554563a0302ae7f434554
Aviroute <youturncorporation@gmail.com>
2022-02-03 19:25:04 -0500

    adding

---
+++ src/homepage.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SOLAR</h1>
+                    <span className='subtitle'>SHOP</span> 
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>PARTS</h1>
+                    <span className='subtitle'>SHOP</span> 
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>CRYPTO</h1>
+                    <span className='subtitle'>SHOP</span> 
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SOLAR</h1>
+                    <span className='subtitle'>SHOP</span> 
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SOLAR</h1>
+                    <span className='subtitle'>SHOP</span> 
+                </div>
+            </div>
+        </div>
+    </div>
+)
