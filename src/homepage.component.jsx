60f9dbcf07d9d2f158dd41dbeac8d3fb54179be5
Aviroute <youturncorporation@gmail.com>
2022-02-03 23:29:52 -0500

    adding

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -23,7 +23,7 @@ const HomePage = () => (
             </div>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>SOLAR</h1>
+                    <h1 className='title'>ABOUT US</h1>
                     <span className='subtitle'>SHOP</span> 
                 </div>
             </div>
